eb2909e08d227d7e1651a39a8865c572a2201453
BigFungus10 <rohanflyyn2009@gmail.com>
2025-09-27 12:33:44 +1000

    fixed bug

--- daydream-project-1/player.gd
+++ daydream-project-1/player.gd
@@ -12,5 +12,5 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 	get_tree().change_scene_to_file("res://shop.tscn")# Replace with function body.
