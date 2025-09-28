c8a0e60a69df3053be82482f4949b47f9318e055
Julius-H747 <julius.hold@gmail.com>
2025-09-28 11:21:40 +1000

    added souls

--- daydream-project-1/soul.gd
+++ daydream-project-1/soul.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
-signal collected
+signal collected  # signal for when the coin is collected
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	
-func _on_body_entered(body: Node) -> void:
-	if body.name == "Player 3":
-			emit_signal("collected")
-			queue_free()
+	$AnimatedSprite2D.play("default")
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	if body.name == "Player 3":  # only trigger if Player touches it
+		emit_signal("collected")  
+		queue_free()  # remove the coin from the scene
